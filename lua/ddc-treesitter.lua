ddb039e86ed4da75127c5df3d5d22f6faaf25b27
delphinus <me@delphinus.dev>
2021-09-05 21:37:02 +0900

    Show ancestors in menu

--- lua/ddc-treesitter.lua
+++ lua/ddc-treesitter.lua
@@ -7,11 +7,23 @@ M.gather_candidates = function()
   for _, tree in pairs(parser:parse()) do
     local query = vim.treesitter.get_query(parser:lang(), 'Highlights')
     for i, node in query:iter_captures(tree:root(), 0) do
+      local parent = node:parent()
+      local grandparent = parent and parent:parent() or nil
       local word = vim.treesitter.get_node_text(node, 0)
       if word then
+        local menu = nil
+        if parent then
+          local text = vim.treesitter.get_node_text(parent, 0)
+          if text then menu = '← '..text end
+        end
+        if grandparent and menu then
+          local text = vim.treesitter.get_node_text(grandparent, 0)
+          if text then menu = menu..' ← '..text end
+        end
         table.insert(candidates, {
           word = word,
           kind = query.captures[i],
+          menu = menu,
         })
       end
     end
