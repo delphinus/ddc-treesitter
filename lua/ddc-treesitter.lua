f9830b0f4dc6e92a93b4d7e701f2ecb04a30b6e4
delphinus <me@delphinus.dev>
2022-01-06 19:26:11 +0900

    Style by stylua

--- lua/ddc-treesitter.lua
+++ lua/ddc-treesitter.lua
@@ -3,9 +3,13 @@ local M = {}
 M.gather_candidates = function()
   local candidates = {}
   local ok, parser = pcall(vim.treesitter.get_parser)
-  if not ok then return candidates end
-  local query = vim.treesitter.get_query(parser:lang(), 'highlights')
-  if not query then return candidates end
+  if not ok then
+    return candidates
+  end
+  local query = vim.treesitter.get_query(parser:lang(), "highlights")
+  if not query then
+    return candidates
+  end
   for _, tree in pairs(parser:parse()) do
     for i, node in query:iter_captures(tree:root(), 0) do
       local parent = node:parent()
@@ -16,8 +20,7 @@ M.gather_candidates = function()
           word = word,
           kind = query.captures[i],
           parent = parent and vim.treesitter.get_node_text(parent, 0) or nil,
-          grandparent = grandparent and
-            vim.treesitter.get_node_text(grandparent, 0) or nil,
+          grandparent = grandparent and vim.treesitter.get_node_text(grandparent, 0) or nil,
         })
       end
     end
